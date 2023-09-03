ea248dbecb42091cae3112590b11981d53f1225f
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-03 20:49:50 +0530

    Added : pre-render in dynamic pages

--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { getEventById } from "@/dummy-data";
+import { getEventById, getAllEvents } from "../../../helpers/api-utils";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-content";
 import ErrorAlert from "@/components/ui/error-alert";
 import Button from "@/components/ui/button";
 
-const EventDetailPage = () => {
-  const router = useRouter();
+const EventDetailPage = (props) => {
+  // const router = useRouter();
 
-  const eventId = router.query.eventId;
+  // const eventId = router.query.eventId;
 
-  const event = getEventById(eventId);
+  // const event = getEventById(eventId);
+
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -46,5 +48,30 @@ const EventDetailPage = () => {
     </React.Fragment>
   );
 };
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+// For which parameter values or for which eventId's next.js should pre-render the page
+
+// And for which id's it should cal  getStaticProps function and the component function
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
 
 export default EventDetailPage;
