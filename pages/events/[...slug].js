fa27842e455d642074866d6f33e3dd756e56e87b
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-07 21:45:47 +0530

    Added : getServerSideProps to catch all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,27 +1,30 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "@/dummy-data";
+// import { getFilteredEvents } from "@/dummy-data";
+
 import EventList from "@/components/events/event-list";
 import ResultsTitle from "@/components/events/results-title";
 import ErrorAlert from "@/components/ui/error-alert";
 import Button from "@/components/ui/button";
+import { getFilteredEvents } from "@/helpers/api-utils";
 
-const FilteredEvent = () => {
+const FilteredEvent = (props) => {
   const router = useRouter();
 
-  const filterData = router.query.slug;
+  // const filterData = router.query.slug;
 
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
-  }
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>;
+  // }
 
-  const year = filterData[0];
-  const month = filterData[1];
+  // const year = filterData[0];
+  // const month = filterData[1];
 
-  const numYear = +year;
-  const numMonth = +month;
+  // const numYear = +year;
+  // const numMonth = +month;
 
-  if (isNaN(numYear) || isNaN(numMonth) || numMonth < 1 || numMonth > 12) {
+  // if (isNaN(numYear) || isNaN(numMonth) || numMonth < 1 || numMonth > 12) {
+  if (props.hasError == true) {
     return (
       <React.Fragment>
         <ErrorAlert>Invalid Filter</ErrorAlert>
@@ -40,7 +43,8 @@ const FilteredEvent = () => {
     );
   }
 
-  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  // const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -61,7 +65,8 @@ const FilteredEvent = () => {
     );
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  // const date = new Date(numYear, numMonth - 1);
+  const date = new Date(props.date.year, props.date.month - 1);
 
   return (
     <div>
@@ -71,4 +76,46 @@ const FilteredEvent = () => {
   );
 };
 
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
+
+  const year = filterData[0];
+  const month = filterData[1];
+
+  const numYear = +year;
+  const numMonth = +month;
+
+  if (isNaN(numYear) || isNaN(numMonth) || numMonth < 1 || numMonth > 12) {
+    return {
+      // notFound: true,
+      // redirects to a error page if page is not found, and we have a page to show error
+      // redirect : {
+      //   destination : "/error-page"
+      // }
+
+      // ---we can also use other ways to show error page when page is not found
+      // return props object and handle hasError in component
+      props: {
+        hasError: true,
+      },
+    };
+  }
+
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  };
+}
 export default FilteredEvent;
